c39471f7d93b857cb961f6bc1af57a22c2408da0
YOON-CC <whdbscks77@gmail.com>
2024-03-14 20:55:33 +0900

    feat: 카테고리 페이지 구현 (#24)

--- src/app/(commonAccess)/(main)/layout.tsx
+++ src/app/(commonAccess)/(main)/layout.tsx
@@ -1,11 +1,13 @@
 import {ReactNode} from "react";
 import Header from "@/components/layout/Header"
+import BottomHeader from "@/components/layout/BottomHeader";
 type Props = { children: ReactNode};
 
 export default function Layout({ children}: Props) {
   return (
     <div>
       <Header/>
+      <BottomHeader />
       {children}
     </div>
   )
