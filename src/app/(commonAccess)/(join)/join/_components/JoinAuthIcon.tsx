afba0b9f170b1f1056d2e8b14d7e6a65a6aac6a6
YOON-CC <whdbscks77@gmail.com>
2024-03-14 13:31:16 +0900

    feat: props를 통해 회원가입 페이지 컴포넌트 재사용 (#20)

--- src/app/(commonAccess)/(join)/join/_components/JoinAuthIcon.tsx
+++ src/app/(commonAccess)/(join)/join/_components/JoinAuthIcon.tsx
@@ -5,6 +5,7 @@ import authIcon1 from '@/assets/image/authIcon/authIcon1.png'
 import authIcon2 from '@/assets/image/authIcon/authIcon2.png'
 import authIcon3 from '@/assets/image/authIcon/authIcon3.png'
 import authIcon6 from '@/assets/image/authIcon/authIcon6.png'
+import Link from "next/link";
 
 export default function SignInAuthIcon() {
 
@@ -15,7 +16,8 @@ export default function SignInAuthIcon() {
             <div style={{width: '100%', display:'flex', justifyContent:'space-evenly',paddingTop: '40px', paddingBottom: '40px'}}>
                 <Image src={authIcon6} alt='' style={{width:'54px', height:'73px'}}></Image>
                 <Image src={authIcon1} alt='' style={{width:'53px', height:'73px'}}></Image>
-                <Image src={authIcon2} alt='' style={{width:'53px', height:'73px'}}></Image>
+                {/* 임시로, 라우트 되도록 구현 => 이후 auth 적용해서 로직 수정 필요 */}
+                <Link href={'/join/easy'}><Image src={authIcon2} alt='' style={{width:'53px', height:'73px'}}></Image></Link>
                 <Image src={authIcon3} alt='' style={{width:'53px', height:'73px'}}></Image>
             </div>
         </>
