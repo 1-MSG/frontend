c5a03619c3bcc3d9d592f719c1a9d8088f28e4b6
yunii23 <yunii23@naver.com>
2024-04-01 12:25:19 +0900

    feat: 리뷰 등록 - 별점 구현, css 수정 (#68)

--- src/app/(products)/product-review/create/page.tsx
+++ src/app/(products)/product-review/create/page.tsx
@@ -1,17 +1,27 @@
 import BtnBack from '@/components/pages/product-review/BtnBack'
 import CreateReview from '@/components/pages/product-review/createReview'
+import reviewData from '@/dummydata/review_create_data.json'
+import { ReviewCreateType } from '@/types/reviewCreateType';
 
-export default function Page() {
+async function fetchData(): Promise<ReviewCreateType[]>{
+    // fetch data
+    const res = reviewData;
+    return res.creat_review;
+}
+
+export default async function Page() {
+
+    const data:ReviewCreateType[] = await fetchData();
 
     return (
         <main className='w-full h-auto'>
             <div className="h-[50px] justify-between flex leading-[2.7rem] border-b border-[#ededed]">
-                <div className='pt-[15px] pl-[10px]'><BtnBack /></div>
+                <div className='pl-[10px] w-[56px] place-self-center'><BtnBack /></div>
                 <h2 className='text-[15px] leading-[3rem] font-[600]'>리뷰 쓰기</h2>
                 <div className='flex items-center w-[56px] h-[50px]'></div>
             </div>
 
-            <CreateReview/>
+            <CreateReview data={data}/>
         </main>
     )
 }
---
+++ src/types/reviewCreateType.ts
@@ -0,0 +1,7 @@
+export interface ReviewCreateType {
+    id: number;
+    order_id: number;
+    product_id: number;
+    product_name: string;
+    product_img_url: string;
+}
