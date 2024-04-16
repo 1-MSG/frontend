c88552ff738d0dd8b40b0b25aed44998a895322e
SEOIHYEON <tjwn1408@naver.com>
2024-04-16 11:35:08 +0900

    refactor: 홈화면 장바구니 버튼 클릭 시 상품상세로 redirect#118

--- src/components/pages/product-list/ProductCardTypeItem2.tsx
+++ src/components/pages/product-list/ProductCardTypeItem2.tsx
@@ -4,15 +4,10 @@ import Image from "next/image"
 import CartIcon from "@/images/svgs/CartIcon";
 import GrayStar from "@/images/svgs/GrayStar";
 import { CommonDataResType } from "@/types/commonResType";
-import { redirect, useRouter } from 'next/navigation';
-import { getServerSession } from "next-auth";
-import { options } from "@/app/api/auth/[...nextauth]/options";
 import LikeEvent from "@/components/pages/product-list/LikeEvent";
-import { useEffect, useState } from "react";
 import useSWR from "swr";
 import { useSession } from 'next-auth/react'
-import cardimg from "@/assets/image/maincard/card2.png"
-import Like from '@/images/svgs/Like';
+import { useRouter } from 'next/navigation';
 
 const fetcher = (args: any) => fetch(args).then(res => res.json())
 function useProduct(productId: number) {
@@ -42,6 +37,7 @@ export default function ProductCardTypeItem2({ productId }: { productId: number 
     const session = useSession();
     const { data, isError } = useProduct(productId)
     const {ImageValue, ImageError} = useImage(productId)
+    const router = useRouter();
 
     if (isError || ImageError) return <div>Failed to load</div>
     if (!data || !ImageValue) return <div>Loading...</div>
@@ -78,6 +74,13 @@ export default function ProductCardTypeItem2({ productId }: { productId: number 
         )
     }
 
+    function handleClick() {
+        if (confirm('상품 상세에서 옵션을 선택 후 장바구니 담기를 진행해주세요!')){
+            router.push(`/product-detail?productId=${productId}`);
+        }
+        return
+    }
+
 
     return (
         <div className='pt-[10px] pb-[20px]'>
@@ -94,7 +97,8 @@ export default function ProductCardTypeItem2({ productId }: { productId: number 
                 <div className='flex'>
                     <LikeEvent productId={productId} accessToken={accessToken}/>
                     {/* <Like w={20} h={20} /> */}
-                    <button className='mr-[3px]'><CartIcon w={26} h={26} /></button>
+                    {/* <Link href={`/product-detail?productId=${productId}`} className='mr-[3px]'><CartIcon w={26} h={26} /></Link> */}
+                    <div className='mr-[3px]' onClick={handleClick}><CartIcon w={26} h={26} /></div>
                 </div>
             </div>
 
